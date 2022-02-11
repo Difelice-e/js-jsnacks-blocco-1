69a836ca2a30ef596b36639e5598062f6c5b8273
Difelice-e <difelice.emanuele@gmail.com>
2022-02-11 16:36:02 +0100

    snack-7 completato

---
+++ snack7/app.js
@@ -0,0 +1,9 @@
+// Stampa le potenze di 2 fino a 1000.
+ 
+const potenzeDue = [];
+for (i = 0; i <= 1000; i++) {
+    const potenzaDue = Math.pow( 2, i);
+    potenzeDue.push(potenzaDue);
+}
+
+console.log(potenzeDue);
