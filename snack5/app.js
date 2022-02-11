941ae1859a5beac8cc13be33fd398e5a0879c508
Difelice-e <difelice.emanuele@gmail.com>
2022-02-11 16:21:40 +0100

    snack-5 completato

--- snack5/app.js
+++ snack5/app.js
@@ -1,2 +1,15 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire 
 // un numero, se è dispari inseriscilo nell’array
+
+const numeri = [];
+
+for ( i = 0; i < 6; i++) {
+
+    const numero = parseFloat( prompt("inserisci un numero!") );
+    
+    if( !isNaN(numero) && numero % 2 != 0) {
+        numeri.push(numero);
+    }
+};
+
+console.log(numeri);
