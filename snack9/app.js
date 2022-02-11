77b4969b61237ff0ded64dfc1a2ec97c9aa36372
Difelice-e <difelice.emanuele@gmail.com>
2022-02-11 17:03:28 +0100

    snack-9 completato

---
+++ snack9/app.js
@@ -0,0 +1,10 @@
+// Calcola la somma e la media dei primi 10 numeri.
+let somma = 0;
+
+for (i = 0; i < 10; i++) {
+    somma = somma + i;
+};
+console.log(somma);
+
+let media = somma / 10;
+console.log(media);
